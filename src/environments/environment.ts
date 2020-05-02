4795291331686c923413cfd0abe55d88db440b15
Allison <snak_one@hotmail.com>
2020-05-02 11:06:55 +0200

    fix(init): removed environments

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  keys: {
-    push: 'BBLiOnKY2ZADekUk2DjlEUXM42nFWIMzJIQ98js1iMnvj8H4qcGIxKKVaKmR0GKAHYyRUMobzkXnVK-w1qhvmFc',
-    google: '143127854086-78o03sprd1k7jqeq53i6r9pe2df64vgv.apps.googleusercontent.com'
-  },
-  api: 'http://localhost:3000/'
-};
